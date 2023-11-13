ea1682b9cfa07dfa755c5d58d1cb772b807ef499
EvaBartolome <eva.bartolome@mrjaffesclass.com>
2023-11-13 11:35:02 -0800

    values.length

--- Downloads/ScannerReadCSVFile-master 3/ScannerReadCSVFile.java
+++ Downloads/ScannerReadCSVFile-master 3/ScannerReadCSVFile.java
@@ -8,7 +8,7 @@ public class ScannerReadCSVFile {
         Scanner scanner = new Scanner(dataFile);  // Instantiate a Scanner object that uses the file
         scanner.useDelimiter("\n"); //Set the delimiter as new-line character,read data one line at a time
 
-        System.out.println("Class: Average score"); //print header
+        System.out.println("Class: Avg score"); //print header
 
         scanner.nextLine(); //skip header line
 
@@ -16,16 +16,16 @@ public class ScannerReadCSVFile {
             String line = scanner.nextLine(); // line reads the next line from file w/ scanner object--stores the entire line as a single string in the line variable. 
             String[] values = line.split(","); //splits line into an array of strings, line of comma-separated numbers representing 10 test scores for each class
 
-            if (values.length >= 11) {
+            if (values.length >= 11) { //ensures that there is enough data in the values array to process
                 String classNumber = values[0]; //extracts the class number from the values array
                 int totalScore = 0; //accumulate the sum of the 10 test scores for the class section (in loop, test scores are added to calculate the overall score for the class section)
 
-                for (int i = 1; i < 11; i++) { // iterates through 10 test scores for class section--adds them to the totalScore variable
+                for (int i = 1; i < values.length; i++) { // iterates through 10 test scores for class section--adds them to the totalScore variable
                   totalScore += Integer.parseInt(values[i]); //convert the string representation of the test score to an integer and adds it to the totalScore variable
                   //i is index to access individual test scores within the values array. values[i] retrieves string representation of test score at index i in values array
                 }
 
-                int averageScore = totalScore / 10; //calculates average of scores
+                int averageScore = totalScore / values.length; //calculates average of scores
 
                 System.out.println(classNumber + ": " + averageScore); //prints average score with class
             }
