53bb5e1546059330750f7118b06aff6d11e598b0
EvaBartolome <eva.bartolome@mrjaffesclass.com>
2023-11-14 10:40:33 -0800

    values.length

--- Downloads/ScannerReadCSVFile-master 3/ScannerReadCSVFile.java
+++ Downloads/ScannerReadCSVFile-master 3/ScannerReadCSVFile.java
@@ -6,26 +6,26 @@ public class ScannerReadCSVFile {
     public void run() throws FileNotFoundException {
         File dataFile = new File("TestScoresByClass.csv"); // Instantiate a File object
         Scanner scanner = new Scanner(dataFile);  // Instantiate a Scanner object that uses the file
-        scanner.useDelimiter("\n"); //Set the delimiter as new-line character,read data one line at a time
+        scanner.useDelimiter("\n"); //Set the delimiter as new-line character, read data one line at a time
 
         System.out.println("Class: Avg score"); //print header
 
         scanner.nextLine(); //skip header line
 
-        while (scanner.hasNext()) {
+        while (scanner.hasNext()) { //continues as long as there is data in file
             String line = scanner.nextLine(); // line reads the next line from file w/ scanner object--stores the entire line as a single string in the line variable. 
-            String[] values = line.split(","); //splits line into an array of strings, line of comma-separated numbers representing 10 test scores for each class
+            String[] values = line.split(","); //creates new array of strings named values, splits line into an array of strings representing 10 test scores for each class
 
-            if (values.length >= 11) { //ensures that there is enough data in the values array to process
+            if (values.length >= 11) { //ensures that there is at least 11 values of data in the values array to process
                 String classNumber = values[0]; //extracts the class number from the values array
                 int totalScore = 0; //accumulate the sum of the 10 test scores for the class section (in loop, test scores are added to calculate the overall score for the class section)
 
                 for (int i = 1; i < values.length; i++) { // iterates through 10 test scores for class section--adds them to the totalScore variable
-                  totalScore += Integer.parseInt(values[i]); //convert the string representation of test score to integer and adds it to the totalScore variable
-                  //i is index to access individual test scores within the values array. values[i] retrieves string representation of test score at index i in values array
+                    totalScore += Integer.parseInt(values[i]); //convert the string representation of test score to integer and adds it to the totalScore variable
+                    //i is index to access individual test scores within the values array. values[i] retrieves string representation of test score at index i in values array
                 }
 
-                int averageScore = totalScore / values.length; //calculates average of scores
+                int averageScore = Math.round((float) totalScore / (values.length - 1)); //calculates average of scores, division in floating-point arithmetic, Math.round--the result to nearest whole number
 
                 System.out.println(classNumber + ": " + averageScore); //prints average score with class
             }
